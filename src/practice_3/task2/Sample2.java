a808b2d763cd75ecf40dbfc03363ae08d5d4655f
Denis Tkachev <allpost-s@ya.ru>
2023-04-24 20:26:12 +0300

    добавил домашнее задание 3го семинара

--- src/practice_3/task2/Sample2.java
+++ src/practice_3/task2/Sample2.java
@@ -10,10 +10,10 @@ public class Sample2 {
     public static Random random = new Random();
 
     //дз2 - Доработать метод generateEmployee(). Метод должен возвращать
-    //любого случайного сотрудника разного типа (Worker, Freelancer)
+    //любого случайного сотрудника разного типа (Worker, Freelancer) - done
 
     //метод generateEmployee() для формирования случайного рабочего
-    public static Worker generateEmployee(){
+    public static Employee generateEmployee(){
 
         String [] names = new String[] {"Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Клим", "Панкратий", "Рубен", "Дементий", "Енот"};
         String [] surnames = new String[] {"Полоскун", "Вассерман", "Никитин", "Козлов", "Шубин", "Волоков", "Копылов", "Семачков", "Лыткин", "Соклов"};
@@ -22,17 +22,26 @@ public class Sample2 {
         int birthyear = Sample2.random.nextInt(1980, 2020);
 
         int [] typeEmployee = new int[] {1, 2};
-//        int salary = random.nextInt(900, 1500);
-//        int salaryIndex = random.nextInt(28, 31);
 
+        int salary = random.nextInt(900, 1500);
+        int salaryIndex = random.nextInt(28, 31);
+
+        int rate = Sample2.random.nextInt(200, 500);
+        int hoursWorking = Sample2.random.nextInt(3, 8);
+        int daysWorking = Sample2.random.nextInt(3, 20);
+        double salaryF = rate * hoursWorking * daysWorking;
 
         int typeEmp = typeEmployee[random.nextInt(2)];
 
         if (typeEmp == 1) {
-            return new Worker(names[random.nextInt(10)], surnames[random.nextInt(10)],  "Worker", birthyear, calculateSalary(), comments[random.nextInt(10)]);
+            return new Worker(names[random.nextInt(10)], surnames[random.nextInt(10)], birthyear, salary*salaryIndex, comments[random.nextInt(10)]);
         }
-        else return new Freelancer(names[random.nextInt(10)], surnames[random.nextInt(10)],  "Worker", birthyear, generateEmployee().calculateSalary(), comments[random.nextInt(10)]);
+        else {
+            return new Freelancer(names[random.nextInt(10)], surnames[random.nextInt(10)],  birthyear, salaryF, comments[random.nextInt(10)]);
+        }
     }
+
+
 
     public static void main(String[] args) {
 
@@ -52,10 +61,10 @@ public class Sample2 {
 
 
 // Сортировка с использованием метода Comparator:
-        //Arrays.sort(employees, new SalaryComparator());
+        Arrays.sort(employees, new BirthYearComparator());
 
         // Сортировка с использованием интерфейса Comparable, имплементированного к калассу Employee:
-        Arrays.sort(employees);
+        //Arrays.sort(employees);
 
 
         System.out.println("\n*** Sorted list of employees: ***\n");
@@ -70,7 +79,7 @@ public class Sample2 {
 //Создание класса SalaryComparator на базе интерфейса Comparator для сортировки нашего списка Employee
 class SalaryComparator implements Comparator<Employee>{
 
-    //принцып сравнения:
+    //принцип сравнения:
     //если о1 больше о2 -->1
     //если объекты равны -->0
     //если о1 меньше о2 -->-1
@@ -92,22 +101,27 @@ class SurNameComparator implements Comparator<Employee>{
         return o1.surName.compareTo(o2.surName);
     }
 }
+//Сотировка по году рождения:
+class BirthYearComparator implements Comparator<Employee>{
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Integer.compare(o1.birthYear, o2.birthYear);
+    }
+}
 
 
 
-abstract class Employee implements Comparable<Employee>{ // имплементирование интерфейса Comparable
+abstract class Employee{ //implements Comparable<Employee>{ // имплементирование интерфейса Comparable
 
     protected String firstName;
     protected String surName;
-    protected String empType;
     protected int birthYear;
     protected double salary;
     protected String comment;
-    //добавить еще поля - дз done
-    public Employee(String firstName, String surName, String empType, int birthYear, double salary, String comment) {
+
+    public Employee(String firstName, String surName, int birthYear, double salary, String comment) {
         this.firstName = firstName;
         this.surName = surName;
-        this.empType = empType;
         this.birthYear = birthYear;
         this.salary = salary;
         this.comment = comment;
@@ -123,15 +137,10 @@ abstract class Employee implements Comparable<Employee>{ // имплементирование ин
     }
 }
 
-
 class Worker extends Employee{
 
-//    public Worker(String firstName, String surName, double salary) {
-//        super(firstName, surName, salary);
-//    }
-
-    public Worker(String firstName, String surName, String empType, int birthYear, double salary, String comment) {
-        super(firstName, surName, empType, birthYear, salary, comment);
+    public Worker(String firstName, String surName, int birthYear, double salary, String comment) {
+        super(firstName, surName, birthYear, salary, comment);
     }
 
     @Override
@@ -143,20 +152,19 @@ class Worker extends Employee{
 
     @Override
     public String toString() {
-        return String.format("%s %s; Рабочий; Год рождения: %s; Средняя месячная зарплата: %.2f; Комментарий: %s",
+        return String.format("%s %s; Worker; Год рождения: %s; Средняя месячная зарплата: %.2f; Комментарий: %s",
                 surName, firstName, birthYear, calculateSalary(), comment);
     }
 
-    @Override
-    public int compareTo(Employee o) {
-        return Double.compare(this.calculateSalary(), o.calculateSalary());
-    }
+//    @Override
+//    public int compareTo(Employee o) {
+//        return Double.compare(this.calculateSalary(), o.calculateSalary());
+    //}
 }
 
-//дз1 - разработать класс
 class Freelancer extends Employee{
-    public Freelancer(String firstName, String surName, String empType, int birthYear, double salary, String comment) {
-        super(firstName, surName, empType, birthYear, salary, comment);
+    public Freelancer(String firstName, String surName, int birthYear, double salary, String comment) {
+        super(firstName, surName, birthYear, salary, comment);
     }
 
     @Override
@@ -169,12 +177,14 @@ class Freelancer extends Employee{
     }
 
     @Override
-    public int compareTo(Employee o) {
-        return 0;
+    public String toString() {
+        return String.format("%s %s; Freelancer; Год рождения: %s; Заработная плата за месяц: %.2f; Комментарий: %s",
+                surName, firstName, birthYear, calculateSalary(), comment);
     }
 
-    //расчет зп
-    //сколько часов, дней работал в месяц. salary*18*5
-
-    // дз3 - создать компаратор еще один со своим методом и отсортировать по нему (добавить др и проч)
+//    @Override
+//    public int compareTo(Employee o) {
+//        return Double.compare(this.calculateSalary(), o.calculateSalary());
+//    }
 }
+
